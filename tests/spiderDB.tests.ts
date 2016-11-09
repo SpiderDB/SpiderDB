b0cc9823cc2c8338365d36a9940abbd288ebbd91
Matthew York <my3681@gmail.com>
2016-11-09 16:04:03 -0600

    - Added test files - Added more core files

---
+++ tests/spiderDB.tests.ts
@@ -0,0 +1,11 @@
+import * as _ from "underscore";
+import {helloWorld} from "./test";
+
+// Example of importing and using a node package:
+// yarn add underscore
+// yarn add @types/underscore
+_.map(["Hello World"], z => console.log(z));
+
+helloWorld();
+
+console.log("Hello World");
